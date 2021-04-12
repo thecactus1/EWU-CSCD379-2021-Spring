0dcbde3e25680912ea12f0508162f583077cbd96
Micah Johnson <65679128+thecactus1@users.noreply.github.com>
2021-04-11 23:37:04 -0700

    First Pull commit - Setup
    Just the setup is done for the first pull.

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class HomeController : Controller{
+        public ActionResult Index(){
+            return View();
+        }
+
+        public ActionResult Page(){
+            return View();
+        }
+    }
+}
