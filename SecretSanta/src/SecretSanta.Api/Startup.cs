b5f7a62a2a017008235405cf8a86e8537760ae45
Micah Johnson <65679128+thecactus1@users.noreply.github.com>
2021-05-02 15:51:18 -0700

    First Commit (API setup)

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -19,15 +19,23 @@ namespace SecretSanta.Api
         {
             services.AddSingleton<IUserRepository, UserRepository>();
             services.AddControllers();
+
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //Swagger Generation
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseRouting();
 
