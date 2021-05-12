65b7076e44e02ff4b062a6dac271ff836c71868c
Micah Johnson <65679128+thecactus1@users.noreply.github.com>
2021-05-12 04:25:02 -0700

    Update Startup.cs

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -12,7 +12,7 @@ namespace SecretSanta.Web
     {
         private static HttpClient UsersHttpClient { get; } = new()
         {
-            BaseAddress = new Uri("https://mjohnson-secretsanta-api.azurewebsites.net/")
+            BaseAddress = new Uri("https://mjohnson-secretsanta-api1.azurewebsites.net")
         };
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
