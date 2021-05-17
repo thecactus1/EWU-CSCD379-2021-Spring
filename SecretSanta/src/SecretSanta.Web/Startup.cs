e2a269e2e6372be29fef7e031aff52f8b105dd84
Micah Johnson <65679128+thecactus1@users.noreply.github.com>
2021-05-16 23:59:51 -0700

    Commit 2
    Tests not working. Thursday part not complete.

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Web.Api;
@@ -10,10 +11,21 @@ namespace SecretSanta.Web
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public System.Net.Http.HttpClient ApiClient { get; }
         private static HttpClient UsersHttpClient { get; } = new()
         {
             BaseAddress = new Uri("https://localhost:5101/")
         };
+
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+            ApiClient = new(){
+                BaseAddress = new Uri(Configuration["ApiHost"])
+            };
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
--- SecretSanta/test/SecretSanta.E2E/EndToEnd/EndToEndTests.cs
+++ SecretSanta/test/SecretSanta.E2E/EndToEnd/EndToEndTests.cs
@@ -1,24 +1,199 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using PlaywrightSharp;
+using System.Linq;
 
 namespace SecretSanta.Web.Tests{
     [TestClass]
     public class EndToEndTests{
 
+        private static WebHostServerFixture<SecretSanta.Web.Startup, SecretSanta.Api.Startup> _Server;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            _Server = new();
+        }
+
         [TestMethod]
         public async Task LaunchHomepage(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
             using var playwright = await Playwright.CreateAsync();
             await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
             {
-                Headless = false,
+                Headless = true,
                 SlowMo = 250
             });
 
             var page = await browser.NewPageAsync();
-            var response = await page.GoToAsync("https://localhost:5001");
+            var response = await page.GoToAsync(localhost);
 
             Assert.IsTrue(response.Ok);
+
+            var Header = await page.GetTextContentAsync("body > header > div > a");
+            Assert.AreEqual("SecretSanta", Header);
+        }
+
+        [TestMethod]
+        public async Task LaunchUsers(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+
+            Assert.IsTrue(response.Ok);
+            await page.ClickAsync("text=Users");
+
+            var text = await page.GetTextContentAsync("body > section > section > a > section > div");
+            Assert.AreEqual("Inigo Montoya", text);
+        }
+
+        [TestMethod]
+        public async Task LaunchGroups(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+
+            await page.ClickAsync("text=Groups");
+
+            Assert.IsTrue(response.Ok);
+
+
+            var text = await page.GetTextContentAsync("body > section > section > a > section > div");
+            Assert.AreEqual("GoonSquad", text);
+        }
+
+
+        [TestMethod]
+        public async Task LaunchGifts(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+
+            await page.ClickAsync("text=Gifts");
+
+            Assert.IsTrue(response.Ok);
+
+
+            var text = await page.GetTextContentAsync("body > section > section > a > section > div");
+            Assert.AreEqual("Rat Poison", text);
+        }
+
+        [TestMethod]
+        public async Task AddGift(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+            await page.ClickAsync("text=Gifts");
+
+            var gifts = await page.QuerySelectorAllAsync("body > section > section");
+            var giftsbefore = gifts.Count();
+
+            await page.ClickAsync("text=create");
+
+            await page.TypeAsync("input#Title", "Test Object");
+            await page.TypeAsync("input#Priority", "1");
+            await page.SelectOptionAsync("select#UserId", "2");
+            await page.ClickAsync("text=Create");
+
+            Assert.IsTrue(response.Ok);
+
+            gifts = await page.QuerySelectorAllAsync("body > section > section");
+            var giftsafter = gifts.Count();
+            Assert.AreEqual(giftsbefore+1, giftsafter);
+        }
+
+        [TestMethod]
+        public async Task DeleteGift(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+            
+            await page.ClickAsync("text=Gifts");
+
+            Assert.IsTrue(response.Ok);
+
+            var gifts = await page.QuerySelectorAllAsync("body > section > section");
+            var giftsbefore = gifts.Count();
+
+            page.Dialog += (_, args) => args.Dialog.AcceptAsync();
+
+            await page.ClickAsync("body > section > section:last-child > a > section > form > button");
+            
+
+            gifts = await page.QuerySelectorAllAsync("body > section > section");
+            var giftsafter = gifts.Count();
+            Assert.AreEqual(giftsbefore, giftsafter+1);
+        }
+
+        [TestMethod]
+        public async Task ModifyGift(){
+            var localhost = _Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true,
+                SlowMo = 250
+            });
+
+
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync("localhost/");
+            
+            await page.ClickAsync("text=Gifts");
+
+            Assert.IsTrue(response.Ok);
+
+            var sectionText = await page.GetTextContentAsync("body > section  > section:last-child > a > section > div");
+
+
+            await page.ClickAsync("body > section > section:last-child");
+
+            await page.ClickAsync("input#Title", clickCount:3); // Select all text in the text box
+            await page.TypeAsync("input#Title", "Updated Gift");
+            await page.ClickAsync("text=Update");
+
+            sectionText = await page.GetTextContentAsync("body > section > section:last-child > a > section > div");
+            Assert.AreEqual("Updated Gift", sectionText);
         }
     }
 }
