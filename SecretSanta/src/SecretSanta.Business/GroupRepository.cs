2b10154848ebeb8f96fe9867cdb51bc82dc4375d
Micah Johnson <65679128+thecactus1@users.noreply.github.com>
2021-05-25 01:07:20 -0700

    Creation of Groups

--- SecretSanta/src/SecretSanta.Business/GroupRepository.cs
+++ SecretSanta/src/SecretSanta.Business/GroupRepository.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using SecretSanta.Data;
-using System.Linq;
 
 namespace SecretSanta.Business
 {
     public class GroupRepository : IGroupRepository
     {
-        private Random Random { get; }
-        //private static Random Random { get; } = new();
-        //private ISpeakerService Random {get;}
-        public GroupRepository(Random random)
-        {
-            Random = random ?? throw new ArgumentNullException(nameof(random));
-        }
-
         public Group Create(Group item)
         {
             if (item is null)
@@ -35,9 +26,6 @@ namespace SecretSanta.Business
             return null;
         }
 
-        private Group GetRandomGroup() 
-            => MockData.Groups[Random.Next(MockData.Groups.Count)];
-
         public ICollection<Group> List()
         {
             return MockData.Groups.Values;
@@ -56,6 +44,45 @@ namespace SecretSanta.Business
             }
 
             MockData.Groups[item.Id] = item;
+        }
+
+
+        private void Shuffle<Group>(List<Group> list)
+        {
+            Random rand = new Random();
+            int count = list.Count;
+            while(count > 1)
+            {
+                count--;
+                int randNum = rand.Next(count+1);
+                Group val = list[randNum];
+                list[randNum] = list[count];
+                list[count] = val;
+            }
+        }
+
+        public AssignmentResult GenerateAssignment(int id)
+        {
+            if(!MockData.Groups.ContainsKey(id)) return AssignmentResult.Error("Group not found");
+
+            
+
+            List<User> userList = MockData.Groups[id].Users;
+
+            if(userList.Count < 3) return AssignmentResult.Error("Groups must have at least 3 users");
+
+            Shuffle(userList);
+
+            for(int i = 0; i < userList.Count; i++)
+            {
+                if(i < userList.Count - 1)
+                    MockData.Groups[id].Assignments.Add(new Assignment(userList[i], userList[i+1]));
+                else
+                {
+                    MockData.Groups[id].Assignments.Add(new Assignment(userList[i], userList[0]));
+                }
+            }
+            return AssignmentResult.Success();
         }
     }
 }
